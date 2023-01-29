006419498d4ce131e09ab36a7362bb0beebb7afe
Domantas Petrauskas <domantas@zyro.com>
2023-01-29 16:30:27 +0000

    Add props for loader

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-const circleCount = ref(10);
-const loaderSize = ref(100);
-const circleSize = ref(20);
+export interface Props {
+  circleCount?: number
+  loaderSize?: number
+  circleSize?: number
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  circleCount: 10,
+  loaderSize: 200,
+  circleSize: 40
+})
 
 CSS.registerProperty({
   name: '--hue',
@@ -12,19 +19,19 @@ CSS.registerProperty({
 });
 
 const getCircleTransform = (circleIndex: number) => {
-  const angleBetweenCircles = 360 / circleCount.value;
-  return `rotate(${90 + angleBetweenCircles * circleIndex}deg) translate(${loaderSize.value / 2
+  const angleBetweenCircles = 360 / props.circleCount;
+  return `rotate(${90 + angleBetweenCircles * circleIndex}deg) translate(${props.loaderSize / 2
     }px) rotate(-${90 + angleBetweenCircles * circleIndex}deg)`;
 };
 </script>
 
 <template>
   <div class="loader">
-    <div class="circle-wrapper" v-for="circleIndex in circleCount" :key="circleIndex" :style="{
+    <div class="circle-wrapper" v-for="circleIndex in props.circleCount" :key="circleIndex" :style="{
       transform: getCircleTransform(circleIndex),
     }">
       <div class="circle" :style="{
-        'animation-delay': `0s, -${circleIndex / circleCount
+        'animation-delay': `0s, -${circleIndex / props.circleCount
           }s, -${circleIndex}s`,
       }" />
     </div>
@@ -39,7 +46,6 @@ const getCircleTransform = (circleIndex: number) => {
 }
 
 @keyframes pulse {
-
   0%,
   30% {
     opacity: 0;
@@ -61,21 +67,21 @@ const getCircleTransform = (circleIndex: number) => {
 }
 
 .loader {
-  width: calc(v-bind(loaderSize) * 1px);
-  height: calc(v-bind(loaderSize) * 1px);
-  border-radius: calc(v-bind(loaderSize) * 1px / 2);
+  width: calc(v-bind('props.loaderSize') * 1px);
+  height: calc(v-bind('props.loaderSize') * 1px);
+  border-radius: calc(v-bind('props.loaderSize') * 1px / 2);
   position: relative;
   animation: loader-rotation 2s infinite linear;
   transform-style: preserve-3d;
 }
 
 .circle-wrapper {
-  width: calc(v-bind(circleSize) * 1px);
-  height: calc(v-bind(circleSize) * 1px);
+  width: calc(v-bind('props.circleSize') * 1px);
+  height: calc(v-bind('props.circleSize') * 1px);
   position: absolute;
   top: 50%;
   left: 50%;
-  margin: calc(v-bind(circleSize) * -1px / 2);
+  margin: calc(v-bind('props.circleSize') * -1px / 2);
   transform-style: preserve-3d;
 }
 
@@ -83,7 +89,7 @@ const getCircleTransform = (circleIndex: number) => {
   width: 100%;
   height: 100%;
   background-color: hsl(var(--hue), 40%, 70%);
-  border-radius: calc(v-bind(circleSize) * 1px / 2);
+  border-radius: calc(v-bind('props.circleSize') * 1px / 2);
   animation: loader-rotation 2s infinite linear reverse,
     pulse 1s infinite linear, hue-rotate 10s infinite linear;
 }
